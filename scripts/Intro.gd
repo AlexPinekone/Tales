9d6c392974cd34b158fc7cc517ee5b7d4224a08b
ElSedim <elsedim09@gmail.com>
2025-05-24 12:51:31 -0600

    Update Intro.gd

--- scripts/Intro.gd
+++ scripts/Intro.gd
@@ -13,8 +13,8 @@ var t : float = 0.0;
 ## Called by the state machine on the engine's main loop tick.
 func update(_delta: float) -> void:
 	#Logica que queramos ejecutar durante todo el tiempo independientemente si la transicion ya acabo o no
-	dialoge.position.y = 550 + sin(t)*5.0
-	t+=5.0 * _delta
+	#dialoge.position.y = 550 + sin(t)*5.0
+	#t+=5.0 * _delta
 	#######################################
 	if enteringfinished: #Ejecuta el codigo del estado actual una vez que se haya finalizado la animacion de transicion
 		if(isExiting): #Si se activo la condicion de cambio de estado ejecutar transicion de cambio
@@ -23,6 +23,7 @@ func update(_delta: float) -> void:
 			await animationTransition.animation_finished #Esperar a que la transicion acabe
 			finished.emit(nextState) #cambiar al siguiente estado
 		else:
+			print(dialoge.visible)
 			isExiting = Input.is_action_pressed("ui_accept");
 			print("current state " + name)
 	else:
@@ -31,10 +32,10 @@ func update(_delta: float) -> void:
 	pass
 
 func enter(previous_state_path: String, data := {}) -> void:
-	var dialoguePos = dialoge.position
+	#var dialoguePos = dialoge.position
 	enteringfinished = false
 	visible = true
-	currentDialogue = $Dialogues/Dialogue1 #Dialogo de escena
+	#currentDialogue = $Dialogues/Dialogue1 #Dialogo de escena
 	$Transition.visible = true
 	if animationTransition and animationTransition.has_animation("start_level"):
 		animationTransition.play("start_level")
